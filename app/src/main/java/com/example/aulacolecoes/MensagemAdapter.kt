af001f97595e6e52360aee683d9da5973bcac3b5
Jonas Felipe Quirino <jonas-felipe16@hotmail.com>
2024-07-03 11:41:11 -0300

    recycler view

--- app/src/main/java/com/example/aulacolecoes/MensagemAdapter.kt
+++ app/src/main/java/com/example/aulacolecoes/MensagemAdapter.kt
@@ -3,33 +3,68 @@ package com.example.aulacolecoes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 
-class MensagemAdapter(private val list: List<Mensagem>) :
+class MensagemAdapter(
+    private val action: (String) -> Unit,
+) :
     Adapter<MensagemAdapter.MensagemViewHolder>() {
 
+    private var list = mutableListOf<Mensagem>()
+
+    fun updateListByIndex() {
+        this.list.removeAt(2)
+        notifyItemRemoved(2)
+//        this.list.add(2, Mensagem("Santos", "Almoçar fora?", "12:30"))
+//        this.list.add(Mensagem("Santos 2", "Almoçar fora?", "12:30"))
+//        this.list.add(Mensagem("Santos 3", "Almoçar fora?", "12:30"))
+//        notifyItemRangeChanged(2, 3)
+//        notifyItemInserted(2)
+    }
+
+    fun updateList(list: MutableList<Mensagem>) {
+        this.list = list
+        notifyDataSetChanged()
+    }
+
     inner class MensagemViewHolder(view: View) : ViewHolder(view) {
-        val textNome: TextView = view.findViewById(R.id.nameTextView)
-        val textUltimaMensagem: TextView = view.findViewById(R.id.ultimaMensagemTextView)
+//        val textNome: TextView = view.findViewById(R.id.nameTextView)
+//        val textUltimaMensagem: TextView = view.findViewById(R.id.ultimaMensagemTextView)
 //        val textHorario: TextView = view.findViewById(R.id.horarioTextView)
+
+        val textNome: TextView = view.findViewById(R.id.nameCardTextView)
+        val textUltimaMensagem: TextView = view.findViewById(R.id.descriptionCardTextView)
+        val image: ImageView = view.findViewById(R.id.imageCardView)
+        private val cardView: CardView = view.findViewById(R.id.cardView)
+
+        fun bindView(mensagem: Mensagem) {
+            textNome.text = mensagem.nome
+            textUltimaMensagem.text = mensagem.ultimaMensagem
+
+            cardView.setOnClickListener {
+                action(mensagem.nome)
+            }
+        }
     }
 
     // Criar a visualização
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MensagemViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val itemView = layoutInflater.inflate(R.layout.item_lista, parent, false)
+//        val itemView = layoutInflater.inflate(R.layout.item_lista, parent, false)
+        val itemView = layoutInflater.inflate(R.layout.item_card_view, parent, false)
         return MensagemViewHolder(view = itemView)
     }
 
     // vincular os dados com a view
     override fun onBindViewHolder(holder: MensagemViewHolder, position: Int) {
         val element = list[position]
-        holder.textNome.text = element.nome
-        holder.textUltimaMensagem.text = element.ultimaMensagem
-//        holder.textHorario.text = element.horario
+
+        holder.bindView(element)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/aulacolecoes/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/aulacolecoes/RecyclerViewActivity.kt
@@ -1,28 +1,65 @@
 package com.example.aulacolecoes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var mensagemAdapter: MensagemAdapter
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
 
-        val list = listOf(
+        val list = mutableListOf(
             Mensagem("Jonas", "Olá, tudo bem?", "10:30"),
-            Mensagem("Beto", "Olá, tudo bem?, Como você está?. Podemos verificar amanhã o aplicativo interno da empresa com o pessoal da fábrica. Você vem hoje para a nossa reunião?", "11:50"),
+            Mensagem(
+                "Beto",
+                "Olá, tudo bem?, Como você está?. Podemos verificar amanhã o aplicativo interno da empresa com o pessoal da fábrica. Você vem hoje para a nossa reunião?",
+                "11:50"
+            ),
             Mensagem("Jackson", "Bom dia Pessoal", "09:45"),
             Mensagem("Vera", "Olá, tudo bem?, você vem hoje para a nossa reunião?", "09:45"),
-            Mensagem("Cesar", "Podemos verificar amanhã o aplicativo interno da empresa com o pessoal da fábrica", "09:45"),
+            Mensagem(
+                "Cesar",
+                "Podemos verificar amanhã o aplicativo interno da empresa com o pessoal da fábrica",
+                "09:45"
+            ),
         )
 
         recyclerView = findViewById(R.id.recycler_view)
-        recyclerView.adapter = MensagemAdapter(list = list)
+        button = findViewById(R.id.addItemButton)
+        mensagemAdapter = MensagemAdapter {
+            Toast.makeText(this, "Olá $it", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, ListViewActivity::class.java))
+        }
+        recyclerView.adapter = mensagemAdapter
+        mensagemAdapter.updateList(list = list)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                this,
+                RecyclerView.VERTICAL
+            )
+        )
+        button.setOnClickListener {
+            mensagemAdapter.updateListByIndex()
+//            list.add(
+//                Mensagem(
+//                    "Douglas",
+//                    "Podemos verificar amanhã!",
+//                    "11:45"
+//                ),
+//            )
+//            mensagemAdapter.updateList(list = list)
+        }
     }
 }
